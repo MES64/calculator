03ec2bfd2ff6f9ed7a86473f617bd0bd103dd891
Mark Skelton <markskelton10@gmail.com>
2024-08-09 08:42:08 +0100

    Create ability to parse display string
    Set the variables for both numbers and the operator to the result of
    parsing the display string. Also placed the calculation code into its
    own function for easier reuse and readability.

--- js/main.js
+++ js/main.js
@@ -3,40 +3,44 @@
 function plus(number1, number2) {
     return number1 + number2;
 }
-
 function minus(number1, number2) {
     return number1 - number2;
 }
-
 function multiply(number1, number2) {
     return number1 * number2;
 }
-
 function divide(number1, number2) {
     if (number2 === 0) throw new Error("Divide by zero error! (number2 = 0)");
     return number1 / number2;
 }
 
-let number1 = -10.5;
-let operator1 = "÷";
-let number2 = 0;
-
-let result;
-switch(operator1) {
-    case "+":
-        result = plus(number1, number2);
-        break;
-    case "-":
-        result = minus(number1, number2);
-        break;
-    case "x":
-        result = multiply(number1, number2);
-        break;
-    case "÷":
-        result = divide(number1, number2);
-        break;
-    default:
-        throw new Error("Invalid Operator (operator1): Must be +, -, x, ÷");
+function calculate(number1, number2, operator) {
+    let result;
+    switch(operator) {
+        case "+":
+            result = plus(number1, number2);
+            break;
+        case "-":
+            result = minus(number1, number2);
+            break;
+        case "x":
+            result = multiply(number1, number2);
+            break;
+        case "÷":
+            result = divide(number1, number2);
+            break;
+        default:
+            throw new Error("Invalid Operator (operator1): Must be +, -, x, ÷");
+    }
+    return result;
 }
 
-console.log(result);
+let number1;
+let operator;
+let number2;
+
+let display = "-10.2 - -2";
+
+[number1, operator, number2] = display.split(" ");
+
+console.log(calculate(+number1, +number2, operator));
