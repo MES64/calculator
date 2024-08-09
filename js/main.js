3b686d4157b47e2a9901f456682240391a4807bf
Mark Skelton <markskelton10@gmail.com>
2024-08-09 14:17:23 +0100

    Add input checks to update the display correctly
    When a button is pressed the input is checked to make sure the display
    works as expected for a calculator. So no 2 operators in a row (except
    negative numbers), decimal points always have a number before and after,
    no operators in the middle of numbers, etc.
    
    I used a lot of helper functions to help make the code more
    self-explanatory and organized the functions into sections to make them
    more easy to find.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+//---------- Calculation ----------//
+
 function plus(number1, number2) {
     return number1 + number2;
 }
@@ -35,23 +37,133 @@ function calculate(number1, number2, operator) {
     return result;
 }
 
-function updateDisplay(event) {
+//---------- Button Input Processing: Main Logic ----------//
+
+function processButtonPress(event) {
     const btn = event.target;
-    const classes = Array.from(btn.classList);
-    if (classes.includes("number")) {
-        display.textContent += btn.id;
+
+    let nextDisplayText = display.textContent;
+    if (checkButtonIsNumber(btn)) {
+        nextDisplayText += btn.id;
     }
-    else if (classes.includes("operator")) {
-        display.textContent += ` ${btn.id} `;
+    else if (checkButtonIsOperator(btn)) {
+        nextDisplayText += ` ${btn.id} `;
     }
     else {
         console.log("Special Button Pressed");
+        return;
     }
+    updateDisplay(nextDisplayText);
 }
+
+function updateDisplay(nextDisplayText) {
+    let finalOutput;
+    if (checkLastInputIsNumber(nextDisplayText)) {
+        finalOutput = processInputIfNumber(nextDisplayText);
+    }
+    else {
+        finalOutput = processInputIfOperator(nextDisplayText);
+    }
+    display.textContent = finalOutput;
+}
+
+function processInputIfNumber(nextDisplayText) {
+    const numberAsString = getNumberFromLatestInput(nextDisplayText);
+    if (checkIsValidNumber(numberAsString)) {
+        return nextDisplayText;
+    }
+    if (checkIsStartOfNegativeNumber(numberAsString)) {
+        return removeSpacesAroundMinus();
+    }
+    if (checkIsStartOfDecimal(numberAsString)) {
+        return insertZeroBeforeDecimal(nextDisplayText);
+    }
+    return display.textContent;
+}
+
+function processInputIfOperator(nextDisplayText) {
+    const prevNumberAsString = getNumberFromPreviousInput(nextDisplayText);
+    if (checkIsStartOfNegativeNumber(prevNumberAsString)) {
+        return display.textContent;
+    }
+    if (checkIsStartOfDecimal(prevNumberAsString)) {
+        return insertZeroAfterDecimal(nextDisplayText);
+    }
+    return nextDisplayText;
+}
+
+//---------- Button Input Processing: Small Helper Functions ----------//
+
+// Edit Next Display Text
+
+function insertZeroBeforeDecimal(nextDisplayText) {
+    return nextDisplayText.slice(0, -1) + "0.";
+}
+
+function insertZeroAfterDecimal(nextDisplayText){
+    return nextDisplayText.slice(0, -3) + "0" + nextDisplayText.slice(-3);
+}
+
+function removeSpacesAroundMinus() {
+    return display.textContent + "-";
+}
+
+// Check Number
+
+function checkIsValidNumber(numberAsString) {
+    return !isNaN(+numberAsString);
+}
+
+function checkIsStartOfNegativeNumber(numberAsString) {
+    return numberAsString === "-";
+}
+
+function checkIsStartOfDecimal(numberAsString) {
+    return numberAsString.slice(-1) === ".";
+}
+
+// Get Most Recent Number
+
+function getNumberFromLatestInput(nextDisplayText) {
+    return nextDisplayText
+        .split(" ")
+        .filter((item) => item !== "")
+        .slice(-1)[0];
+}
+
+function getNumberFromPreviousInput(nextDisplayText) {
+    return nextDisplayText
+        .split(" ")
+        .filter((item) => item !== "")
+        .slice(-2, -1)[0];
+}
+
+// Check Which Button Pressed
+
+function checkButtonIsNumber(btn) {
+    const btnClasses = Array.from(btn.classList);
+    return btnClasses.includes("number");
+}
+
+function checkButtonIsOperator(btn) {
+    const btnClasses = Array.from(btn.classList);
+    return btnClasses.includes("operator")
+}
+
+// Check last entry is a number
+
+function checkLastInputIsNumber(nextDisplayText) {
+    const arrayOfInputs = nextDisplayText
+        .split(" ")
+        .filter((item) => item !== "");
+    return arrayOfInputs.length % 2 !== 0;  // Every odd entry is a number input
+}
+
+//---------- Main ----------//
 
 const display = document.querySelector("#display");
 const btns = document.querySelector("#btns");
-btns.addEventListener("click", updateDisplay);
+btns.addEventListener("click", processButtonPress);
 
 let number1;
 let operator;
